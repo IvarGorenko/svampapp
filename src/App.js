98a6e3f91b7ff130fed3a6dd8fe46fd0c20da1cb
IvarGorenko <73782886+IvarGorenko@users.noreply.github.com>
2020-11-01 16:32:36 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg'
+import './App.css'
+
+function App () {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        <h2>
+          Test 123
+        </h2>
+      </header>
+    </div>
+  )
+}
+
+export default App
